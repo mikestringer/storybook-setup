856affd00b0b3fd8bea4c8c80db0a58074f6e15f
mikestringer <mike.stringer@gmail.com>
2026-02-17 14:59:31 -0500

    Update generate_assets.py

--- generate_assets.py
+++ generate_assets.py
@@ -8,8 +8,8 @@ import os
 from PIL import Image, ImageDraw, ImageFont, ImageFilter
 
 # Screen dimensions (rotated for landscape book mode)
-SCREEN_WIDTH = 720
-SCREEN_HEIGHT = 1280
+SCREEN_WIDTH = 1024
+SCREEN_HEIGHT = 600
 
 # Output directory
 ASSETS_DIR = "images"
