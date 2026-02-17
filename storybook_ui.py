40e83c6acb107b12f95907d274d170902c790e74
mikestringer <mike.stringer@gmail.com>
2026-02-17 15:11:31 -0500

    Update storybook_ui.py

--- storybook_ui.py
+++ storybook_ui.py
@@ -61,7 +61,7 @@ REED_SWITCH_ENABLED = False  # Set to True when reed switch is installed
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 1024
 #ROTATION = 0  # 0, 90, 180, or 270
-ROTATION = 0  # Rotate for landscape book mode
+ROTATION = 90  # Rotate for landscape book mode
 
 # Font Settings
 try:
