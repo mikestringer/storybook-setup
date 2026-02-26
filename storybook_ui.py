587796562365e768f7dca9e3c3906d7c8d234d77
mikestringer <mike.stringer@gmail.com>
2026-02-26 09:13:02 -0500

    Update storybook_ui.py

--- storybook_ui.py
+++ storybook_ui.py
@@ -94,7 +94,7 @@ class VoiceListener:
     
     def __init__(self, energy_threshold=300, record_timeout=10):
         print("🎤 Initializing voice listener...")
-        time.sleep(1)  # Give USB/ALSA time to settle
+        time.sleep(.5)  # Give USB/ALSA time to settle
 
         
         # Find USB microphone automatically
